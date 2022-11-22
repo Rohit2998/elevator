73e9645b0ed312c280003b0a40e5bd4ea9f66a0a
Rohit2998 <43453145+Rohit2998@users.noreply.github.com>
2022-11-22 14:24:06 +0530

    views added url and app updated

--- elevator_system/apps.py
+++ elevator_system/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
 
-
 class ElevatorSystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'elevator_system'
+    def ready(self):
+
+        from elevator_system.move_elevator import RunThread
+
+        RunThread().start()
+
--- elevator_system/urls.py
+++ elevator_system/urls.py
@@ -1,12 +1,15 @@
-from django.urls import include, path
+from django.urls import path
 
-from rest_framework import routers
+from .views import *
 
-from elevator_system.views import ElevatorViewSet
+urlpatterns=[
 
-router = routers.DefaultRouter()
-router.register(r'elevator', ElevatorViewSet)
-
-urlpatterns = [
-   path('', include(router.urls)),
+    path('el-sys/list/',ElevatorSystemList.as_view(),name='el-sys-list'),
+    path('el-sys/add-new/',CreateElevatorSystem.as_view(),name='add-new-els'),
+    path('el-sys/<int:id>/list/',ElevatorsList.as_view(),name='elevator-list'),
+    path('el-sys/<int:id>/elevator/<int:pk>/view/',ViewSingleElevator.as_view(),name='elevator-view'),
+    path('el-sys/<int:id>/elevator/<int:pk>/update/',UpdateSingleElevator.as_view(),name='elevator-update'),
+    path('el-sys/<int:id>/elevator/<int:pk>/destination/',FetchDestination.as_view(),name='fetch-destination'),
+    path('el-sys/<int:id>/elevator/<int:pk>/req/add-new/',CreateElevatorRequest.as_view(),name='add-new-req'),
+    path('el-sys/<int:id>/elevator/<int:pk>/req/view/',ElevatorRequestList.as_view(),name='req-list'),
 ]
