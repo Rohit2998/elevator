82dd0483b226ad1d3b71f072c01f63a89c86ee24
Rohit2998 <43453145+Rohit2998@users.noreply.github.com>
2022-11-22 13:21:42 +0530

    Create create_elevators.py

---
+++ elevator_system/create_elevators.py
@@ -0,0 +1,11 @@
+from .models import Elevator
+
+def create_elevators(number_of_elevators : int,system_id : int):
+
+  for i in range(number_of_elevators):
+    elevator_object = Elevator.objects.create(
+      elevator_system_id = system_id,
+      elevator_number = i+1,
+    )
+
+    elevator_object.save()
