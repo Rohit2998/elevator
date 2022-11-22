d2f799bd48cf09b6919fa64e27fe14229543bd88
Rohit2998 <43453145+Rohit2998@users.noreply.github.com>
2022-11-22 12:37:39 +0530

    Update serializers.py

--- elevator_system/serializers.py
+++ elevator_system/serializers.py
@@ -1,7 +1,35 @@
 from rest_framework import serializers
-from elevator_system.models import *
+from .models import ElevatorSystem,Elevator,ElevatorRequest
+
+
+class ElevatorSystemSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = ElevatorSystem
+    fields = '__all__'
+
+
 
 class ElevatorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Elevator
-        fields=('elevatorID','currentfloor','requested_floor','elevator_status')
+
+  class Meta:
+    model = Elevator
+    fields = '__all__'
+
+
+
+class ElevatorRequestSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = ElevatorRequest
+    fields = (
+      'requested_floor', 
+      'destination_floor',
+    )
+
+
+
+class ElevatorRequestSerializerAll(serializers.ModelSerializer):
+  class Meta:
+    model = ElevatorRequest
+    fields = '__all__'
